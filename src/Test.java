0ff46e09084c91fc59c2322a0ad99c533f12d71e
Hunter Dorminey <hdormine@students.kennesaw.edu>
2023-04-10 16:45:11 -0400

    Have image displaying in window now.

--- src/Test.java
+++ src/Test.java
@@ -1,33 +1,32 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
-    public class Test extends JPanel {
+public class Test extends JPanel {
 
-        public void paint(Graphics g) {
-            Image img = createImageWithText();
-            g.drawImage(img, 20,20,this);
-        }
+    public static BufferedImage newIMG;
 
-        private Image createImageWithText() {
-            BufferedImage bufferedImage = new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
-            Graphics g = bufferedImage.getGraphics();
-
-            g.drawString("www.tutorialspoint.com", 20,20);
-            g.drawString("www.tutorialspoint.com", 20,40);
-            g.drawString("www.tutorialspoint.com", 20,60);
-            g.drawString("www.tutorialspoint.com", 20,80);
-            g.drawString("www.tutorialspoint.com", 20,100);
-
-            return bufferedImage;
-        }
-
-        public static void main(String[] args) {
-            JFrame frame = new JFrame();
-            frame.getContentPane().add(new Test());
-
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(200, 200);
-            frame.setVisible(true);
+    static {
+        try {
+            newIMG = ImageIO.read(Objects.requireNonNull(Test.class.getResource("/resources/images/BAJIp.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    public void paint(Graphics g) {
+        g.drawImage(newIMG, 0, 0, this);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Noisy Image");
+        frame.getContentPane().add(new Test());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(newIMG.getWidth(), newIMG.getHeight() + 100);
+        frame.setVisible(true);
+    }
+}
